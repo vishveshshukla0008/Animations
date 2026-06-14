39ee033510f626ef405086f11dab07e2466cbb49
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-06-15 00:10:21 +0530

    Animation-scrolltrigger-image and text

--- Custom/script.js
+++ Custom/script.js
@@ -1,66 +1,28 @@
-const play = document.querySelector(".play");
-const pause = document.querySelector(".pause");
-const restart = document.querySelector(".restart");
-const reverse = document.querySelector(".reverse");
-const seek = document.querySelector(".seek");
-
-const tl = gsap.timeline({ paused: true });
-
-tl.to(".box", {
-  x: 400,
-  duration: 1,
-  delay: 0.6,
-  ease: "power3.inOut",
-})
-  .to(".box1", {
-    x: 400,
-    duration: 1,
-    ease: "power3.inOut",
-  })
-  .to(".box2", {
-    x: 400,
-    duration: 1,
-    ease: "power3.inOut",
-  })
-  .addLabel("yash")
-  .to(".box3", {
-    x: 400,
-    duration: 1,
-    ease: "power3.inOut",
-  })
-  .to(".box4", {
-    x: 400,
-    duration: 1,
-    ease: "power3.inOut",
-  });
-
-play.addEventListener("click", () => {
-  tl.play();
+gsap.set(".image-div", {
+  scale: 0.25,
 });
 
-pause.addEventListener("click", () => {
-  tl.pause();
+gsap.set(".content", {
+  gap: "70rem",
 });
 
-restart.addEventListener("click", () => {
-  tl.restart();
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".image-div",
+    start: "top 50%",
+    end: "top 40%",
+    scrub: true,
+  },
 });
 
-reverse.addEventListener("click", () => {
-  tl.reverse();
-});
-
-seek.addEventListener("click", () => {
-  tl.seek("yash");
-});
-
-const firstTimeLine = () => {
-  return gsap.timeline();
-};
-
-const secondTimeLine = () => {
-  return gsap.timeline();
-};
-
-const master = gsap.timeline();
-master.add(firstTimeLine, "-=0.4").add(secondTimeLine);
+tl.to(".image-div", {
+  scale: 1,
+  ease: "power1.out",
+}).to(
+  ".content",
+  {
+    gap: "5rem",
+    ease: "expo.in",
+  },
+  "-0.25",
+);
