73ee7ccec34421ccc5ed907585c102c73cf7b1a1
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-06-16 15:34:45 +0530

    ANIMATION-FLIP-IMAGE-GALLERY-AND-IMAGE-SHOW

--- Custom/script.js
+++ Custom/script.js
@@ -1,13 +1,41 @@
-Draggable.create(".box", {
-  //   bounds: {
-  //     maxX: 100,
-  //     minX: 20,
-  //     maxY: 80,
-  //     minY: 40,
-  //   },
+const GalleryImages = document.querySelectorAll(".imageGallery img");
 
-  bounds: ".outer",
-  edgeResistance: 1,
-  inertia: true,
-  dragResistance: 0.5,
-});
+console.log(GalleryImages);
+
+// ❌ Problem 1:
+// Ye dono variables tumne bahar bana diye.
+// img2 ek fixed element ko store kar raha hai.
+// Jab pehli baar image swap hogi to img2 gallery me chala jayega,
+// lekin variable ab bhi usi purani image ko refer karega.
+// Flip swap ke case me current image ko dynamically lena chahiye.
+
+// const img = document.querySelector(".speacialImage");
+// const img2 = document.querySelector(".specialImage2");
+
+function dynamicGallery() {
+  GalleryImages.forEach((img) => {
+    img.addEventListener("click", () => {
+      const img2 = document.querySelector(".imageShow img");
+      const state = Flip.getState(img);
+      const state2 = Flip.getState(img2);
+
+      document.querySelector(".imageGallery").appendChild(img2);
+      document.querySelector(".imageShow").appendChild(img);
+
+      Flip.from(state, {
+        duration: 1,
+        ease: "power3.in",
+        absolute: true,
+        scale: true,
+      });
+      Flip.from(state2, {
+        duration: 1,
+        ease: "power3.in",
+        absolute: true,
+        scale: true,
+      });
+    });
+  });
+}
+
+dynamicGallery();
