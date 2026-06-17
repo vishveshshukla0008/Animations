080af0ebcfc19e549bf2ac2f015640ffd936a8bb
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-06-17 21:58:28 +0530

    Added Object variants in passing in motion

--- GSAP-REACT/src/App.jsx
+++ GSAP-REACT/src/App.jsx
@@ -1,15 +1,27 @@
 import "./App.scss";
-import AnimateOnX from "./components/AnimateOnX";
-
+import { motion } from "motion/react";
 const App = () => {
+  const boxVariant = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+    },
+  };
   return (
     <div className="outer">
-      <AnimateOnX>
-        <div className="box"></div>
-      </AnimateOnX>
-      <AnimateOnX>
-        <button>dabao mujhe</button>
-      </AnimateOnX>
+      <div className="page"></div>
+      <div className="page page2">
+        <motion.div
+          initial={boxVariant.hidden}
+          transition={{ duration: 6, ease: "easeOut", mass: 5 }}
+          viewport={{ once: false, amount: 0.25 }}
+          whileHover={boxVariant.visible}
+          whileInView={{ opacity: 1 }}
+          whileFocus={{ backgroundColor: "red" }}
+          className="box"></motion.div>
+      </div>
     </div>
   );
 };
--- GSAP-REACT/src/components/AnimateOnX.jsx
+++ GSAP-REACT/src/components/AnimateOnX.jsx
@@ -2,7 +2,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef } from "react";
 const AnimateOnX = ({ children }) => {
-    const containerRef = useRef(null);
+  const containerRef = useRef(null);
   useGSAP(
     () => {
       gsap.to(containerRef.current, {
